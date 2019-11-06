121794f009e9c2d021ff27847c2980162818a9d1
David Tai <david@verus.io>
2019-11-05 18:52:29 -0600

    add helper for automatically getting the named exports

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,31 @@ import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
 import json from 'rollup-plugin-json'
 
+import fs from 'fs'
+import nodeEval from 'node-eval'
+
+export function getModuleExports(moduleId) {
+    const id = require.resolve(moduleId)
+    const moduleOut = nodeEval(fs.readFileSync(id).toString(), id)
+    let result = []
+    const excludeExports = /^(default|__)/
+    if (moduleOut && typeof moduleOut === 'object') {
+        result = Object.keys(moduleOut)
+            .filter(name => !excludeExports.test(name))
+    }
+
+    return result
+}
+
+// Helper for getting the names exports automagically
+export function getNamedExports(moduleIds) {
+    const result = {}
+    moduleIds.forEach( id => {
+        result[id] = getModuleExports(id)
+    })
+    return result
+}
+
 const plugins = [
   resolve(),
   json({
