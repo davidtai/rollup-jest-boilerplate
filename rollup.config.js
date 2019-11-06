d709468ba55baaa89907d5886bdf804e78d7cefc
David Tai <david@verus.io>
2019-11-05 18:49:27 -0600

    add plugins to all build configs

--- rollup.config.js
+++ rollup.config.js
@@ -4,43 +4,45 @@ import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
 import json from 'rollup-plugin-json'
 
+const plugins = [
+  resolve(),
+  json({
+    // All JSON files will be parsed by default,
+    // but you can also specifically include/exclude files
+    include: 'node_modules/**',
+    exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],
+
+    // for tree-shaking, properties will be declared as
+    // variables, using either `var` or `const`
+    preferConst: true, // Default: false
+
+    // specify indentation for the generated default export —
+    // defaults to '\t'
+    indent: '  ',
+
+    // ignores indent and generates the smallest code
+    compact: true, // Default: false
+
+    // generate a named export for every property of the JSON object
+    namedExports: true // Default: true
+  }),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  commonjs(),
+]
+
 export default [
   // browser-friendly UMD build
   {
     input: 'src/index.js',
+    plugins,
     output: {
       name: 'rollupJestBoilerplate',
       file: pkg.browser,
       format: 'umd',
       sourcemap: true,
     },
-    plugins: [
-      resolve(),
-      json({
-        // All JSON files will be parsed by default,
-        // but you can also specifically include/exclude files
-        include: 'node_modules/**',
-        exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],
-
-        // for tree-shaking, properties will be declared as
-        // variables, using either `var` or `const`
-        preferConst: true, // Default: false
-
-        // specify indentation for the generated default export —
-        // defaults to '\t'
-        indent: '  ',
-
-        // ignores indent and generates the smallest code
-        compact: true, // Default: false
-
-        // generate a named export for every property of the JSON object
-        namedExports: true // Default: true
-      }),
-      babel({
-        exclude: 'node_modules/**',
-      }),
-      commonjs(),
-    ],
   },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
@@ -52,6 +54,7 @@ export default [
   {
     input: 'src/index.js',
     external: [],
+    plugins,
     output: [
       { file: pkg.main, format: 'cjs', sourcemap: true },
       { file: pkg.module, format: 'es', sourcemap: true },
